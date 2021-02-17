b661dd4abb1a925355dabe4411b2c734bb630944
Eduardo Villamor <villamore838@gmail.com>
2021-02-17 10:28:30 -0500

    Change picture of testimonials

--- src/components/utils/TestimonialDB.js
+++ src/components/utils/TestimonialDB.js
@@ -5,22 +5,22 @@ export const testimonialDB = [{
         testimonial: '1Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 150'
     },{
         id: 1,
-        imgCard: 'https://randomuser.me/api/portraits/men/19.jpg',
+        imgCard: 'https://randomuser.me/api/portraits/men/92.jpg',
         name: 'Pepito 1',
         testimonial: '2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 150'
     },{
         id: 2,
-        imgCard: 'https://randomuser.me/api/portraits/men/19.jpg',
+        imgCard: 'https://randomuser.me/api/portraits/men/21.jpg',
         name: 'Pepito 2',
         testimonial: '3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 150'
     },{
         id: 3,
-        imgCard: 'https://randomuser.me/api/portraits/men/19.jpg',
+        imgCard: 'https://randomuser.me/api/portraits/men/23.jpg',
         name: 'Pepito 3',
         testimonial: '4Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 150'
     },{
         id: 4,
-        imgCard: 'https://randomuser.me/api/portraits/men/19.jpg',
+        imgCard: 'https://randomuser.me/api/portraits/men/78.jpg',
         name: 'Pepito 4',
         testimonial: '5Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 150'
     }
